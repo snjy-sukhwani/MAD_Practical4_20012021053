9e13363a45b6901a21de6a4a675f1f37dc993abb
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2022-09-17 20:55:53 +0530

    Update : Adding Binding

--- app/src/main/java/com/example/mad_practical4_20012021053/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical4_20012021053/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.mad_practical4_20012021053
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.example.mad_practical4_20012021053.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar (binding.toolbar)
+
     }
 }
