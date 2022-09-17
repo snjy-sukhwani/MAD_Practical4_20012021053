2ee9799e4b8ac21729dace7cdac06a0657a7d5c1
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2022-09-17 21:29:09 +0530

    Update : Added Web & Call onclick Event with Intent in MainActivity.kt

--- app/src/main/java/com/example/mad_practical4_20012021053/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical4_20012021053/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.mad_practical4_20012021053
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.WindowCompat
 import com.example.mad_practical4_20012021053.databinding.ActivityMainBinding
 
@@ -18,6 +21,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar (binding.toolbar)
+
+        binding.webUrlBtn.setOnClickListener {
+
+            val userWebUrl = binding.WebURLEditText.text.toString()
+
+            Toast.makeText(this,"Redirecting you to $userWebUrl",Toast.LENGTH_LONG).show()
+
+            Intent(Intent.ACTION_VIEW).setData(Uri.parse(userWebUrl)).apply { startActivity(this) }
+        }
+
+        binding.CallPhoneBtn.setOnClickListener{
+
+            val userCallNow = binding.CallPhoneEditText.text.toString()
+
+            if (userCallNow.length==10){
+
+                Toast.makeText(this,"Redirecting you to Your Dialer with $userCallNow Number",Toast.LENGTH_LONG).show()
+
+                Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:$userCallNow")).apply { startActivity(this) }
+            }
+            else
+            {
+                Toast.makeText(this,"Invalid Phone Number",Toast.LENGTH_LONG).show()
+            }
+        }
 
     }
 }
