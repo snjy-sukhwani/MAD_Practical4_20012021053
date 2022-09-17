a6e3bd300e4503c817ed426f7033edc87ebd1ca3
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2022-09-17 23:21:25 +0530

    Update : Added all onclick Events in Kotlin File

--- app/src/main/java/com/example/mad_practical4_20012021053/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical4_20012021053/MainActivity.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.CallLog
+import android.provider.ContactsContract
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.core.view.WindowCompat
 import com.example.mad_practical4_20012021053.databinding.ActivityMainBinding
@@ -26,9 +31,15 @@ class MainActivity : AppCompatActivity() {
 
             val userWebUrl = binding.WebURLEditText.text.toString()
 
-            Toast.makeText(this,"Redirecting you to $userWebUrl",Toast.LENGTH_LONG).show()
+            Toast.makeText(this,"Redirecting you to \"$userWebUrl\"",Toast.LENGTH_LONG).show()
 
-            Intent(Intent.ACTION_VIEW).setData(Uri.parse(userWebUrl)).apply { startActivity(this) }
+            if (userWebUrl.startsWith("https://"))
+            {
+                Intent(Intent.ACTION_VIEW).setData(Uri.parse(userWebUrl)).apply { startActivity(this) }
+            }
+            else{
+                Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://$userWebUrl")).apply { startActivity(this) }
+            }
         }
 
         binding.CallPhoneBtn.setOnClickListener{
@@ -45,6 +56,26 @@ class MainActivity : AppCompatActivity() {
             {
                 Toast.makeText(this,"Invalid Phone Number",Toast.LENGTH_LONG).show()
             }
+        }
+
+        binding.ContactListBtn.setOnClickListener {
+            Intent(Intent.ACTION_VIEW).setType(ContactsContract.Contacts.CONTENT_TYPE).apply { startActivity(this) }
+        }
+
+        binding.CallLogBtn.setOnClickListener{
+            Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).apply { startActivity(this) }
+        }
+
+        binding.GalleryBtn.setOnClickListener{
+            Intent(Intent.ACTION_VIEW).setType("image/*").apply { startActivity(this) }
+        }
+
+        binding.CameraBtn.setOnClickListener{
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply { startActivity(this) }
+        }
+
+        binding.AlarmBtn.setOnClickListener{
+            Intent(AlarmClock.ACTION_SHOW_ALARMS).apply { startActivity(this) }
         }
 
     }
